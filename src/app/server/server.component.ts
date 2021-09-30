61d26c549b8e066e8b1c1d83f0068b75cdf9b409
Sivanantham Padikkasu <pksivanantham@gmail.com>
2021-09-30 23:04:48 +0530

    new components added

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-server',
+    templateUrl:'./server.component.html',
+    styleUrls:['./server.component.scss']//Style urls is an array because we can provide n number of css
+})
+export class ServerComponent {
+
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    //selector:'[app-servers]', /* By attribute selector*/
+    //selector:'.app-servers', /* By class selector*/
+    selector:'app-servers',
+    templateUrl:'./servers.component.html',
+    styleUrls:['./servers.component.scss']//Style urls is an array because we can provide n number of css
+})
+export class ServersComponent {
+
+}
